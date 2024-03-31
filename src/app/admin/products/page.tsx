ce12bcffbdc919c7388d2e8f9de9596ff6eb52a5
sumanacharyyagit <sumanacharyya999@gmail.com>
2024-04-01 00:35:54 +0530

    modified > schema and update > productsTableView

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import PageHeader from "../_components/PageHeader";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import db from "@/db/db";
+import { CheckCircle2, MoreVertical, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const AdminProductPage = () => {
     return (
@@ -18,7 +32,19 @@ const AdminProductPage = () => {
     );
 };
 
-const ProductsTable = () => {
+const ProductsTable = async () => {
+    const products = await db.product.findMany({
+        select: {
+            id: true,
+            name: true,
+            priceInCents: true,
+            isAvailableForPurchase: true,
+            _count: { select: { orders: true } },
+        },
+        orderBy: { name: "asc" },
+    });
+
+    if (products.length === 0) return <p>No products found</p>;
     return (
         <Table>
             <TableHeader>
@@ -34,6 +60,40 @@ const ProductsTable = () => {
                     </TableHead>
                 </TableRow>
             </TableHeader>
+            <TableBody>
+                {products.map((item) => (
+                    <TableRow key={item.id}>
+                        <TableCell>
+                            {item.isAvailableForPurchase ? (
+                                <>
+                                    <span className="sr-only">Available</span>
+                                    <CheckCircle2 />{" "}
+                                </>
+                            ) : (
+                                <>
+                                    <span className="sr-only">unavailable</span>
+                                    <XCircle />
+                                </>
+                            )}
+                        </TableCell>
+                        <TableCell>{item.name}</TableCell>
+                        <TableCell>
+                            {formatCurrency(item.priceInCents / 100)}
+                        </TableCell>
+                        <TableCell>
+                            {formatNumber(item._count.orders)}
+                        </TableCell>
+                        <TableCell>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <MoreVertical />
+                                    <span className="sr-only">Actions</span>
+                                </DropdownMenuTrigger>
+                            </DropdownMenu>
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
         </Table>
     );
 };
